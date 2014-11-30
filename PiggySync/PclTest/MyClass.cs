894c903eb7cced7ff0160a33ca13d4bc124be7b2
wjch-krl <wjch.krl@gmail.com>
2014-11-30 12:35:39 +0100

    CleanUp

--- PiggySync/PclTest/MyClass.cs
+++ PiggySync/PclTest/MyClass.cs
@@ -2,5 +2,13 @@
 {
     public class MyClass
     {
+        public void Test()
+        {
+
+            TcpListener listner = new TcpListener(new IPEndPoint(PiggyRemoteHost.Me.Ip, 1339));
+            listner.Start();
+            TcpClient newConnection;
+            newConnection = listner.AcceptTcpClient();
+        }
     }
 }
