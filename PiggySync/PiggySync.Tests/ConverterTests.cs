5cada29812a051c82442e5b25ae2e3038bc65edd
wjch-krl <wjch.krl@gmail.com>
2015-01-04 19:25:37 +0100

    PCL fixes

--- PiggySync/PiggySync.Tests/ConverterTests.cs
+++ PiggySync/PiggySync.Tests/ConverterTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using PiggySync.Domain;
 using PiggySync.FileMerger;
 
