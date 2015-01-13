303ba8ca905c945cd2fd8d3d3d58f7cdf1bf1ba3
wjch-krl <wjch.krl@gmail.com>
2015-01-13 22:24:39 +0100

    mainform

--- PiggySync/PiggySync.WinApp/MainForm.cs
+++ PiggySync/PiggySync.WinApp/MainForm.cs
@@ -4,35 +4,35 @@ using PiggySync.GuiShared;
 
 namespace PiggySync.WinApp
 {
-    public partial class MainForm : Form, IMainView
-    {
-        private MainPresenter presenter;
+	public partial class MainForm : Form, IMainView
+	{
+		private MainPresenter presenter;
 
-        public MainForm()
-        {
-            InitializeComponent();
-      //      presenter = new MainPresenter(this);
-        }
+		public MainForm ()
+		{
+			InitializeComponent ();
+		     presenter = new MainPresenter(this);
+		}
 
-        public double ProgresLevel
-        {
+		public double ProgresLevel
+		{
 			get { return progressBar1.Value / 100.0; }
 			set { progressBar1.Value = (int)Math.Round (value * 100); }
-        }
+		}
 
-        public SyncStatus SyncStatus
-        {
-			set { statusTextBox.Text = string.Format("Sync Status:\n{0}", value.ToString ()); }
-        }
+		public SyncStatus SyncStatus
+		{
+			set { statusTextBox.Text = string.Format ("Sync Status:\n{0}", value.ToString ()); }
+		}
 
-        private void settingsButton_Click(object sender, EventArgs e)
-        {
-            new SettingsForm().ShowDialog();
-        }
+		private void settingsButton_Click (object sender, EventArgs e)
+		{
+			new SettingsForm ().ShowDialog ();
+		}
 
-        private void hostsButton_Click(object sender, EventArgs e)
-        {
-            new HostsViewForm().ShowDialog();
-        }
-    }
+		private void hostsButton_Click (object sender, EventArgs e)
+		{
+			new HostsViewForm ().ShowDialog ();
+		}
+	}
 }
