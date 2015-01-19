73818d585fd65ebe8e4d228bfb9f9b6abd80f6d6
Wojciech Król <wjch.krl@gmail.com>
2015-01-19 11:57:42 +0100

    Main Fragment

--- PiggySync/PiggySync.AndroidApp/MainFragment.cs
+++ PiggySync/PiggySync.AndroidApp/MainFragment.cs
@@ -5,12 +5,33 @@ using Android.OS;
 
 namespace PiggySync.AndroidApp
 {
-	public class MainFragment: Fragment
+	public class MainFragment: Fragment, IMainView
 	{
+		private MainPresenter presenter;
+		private ProgressBar progresBar;
+		
 		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 		{
 			View view = inflater.Inflate(Resource.Layout.mainlayout, null);
+			this.presenter = new MainPresenter(this);
+			this.progresBar = FindViewById<ProgressBar>(Resource.Id.progressBar1);
 			return view;
+		}
+		
+		public double ProgresLevel 
+		{
+			get { return progresBar.Progress / 100.0; }
+			set { progresBar.Progress = Convert.ToIn32(value * 100);   }
+		}
+
+        public SyncStatus SyncStatus 
+		{ 
+			set { throw new NotImplementedException(); } 
+		}
+
+		public bool ProgresEnabled 
+		{
+			set { throw new NotImplementedException(); } 
 		}
 	}
 }
